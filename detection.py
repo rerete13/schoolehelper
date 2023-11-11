93dcd1ccf2be33dee6764f7eea7fa398302acd62
Taras Krupa <taraskrupa@pc-192-168-1-103.p.lodz.pl>
2023-11-12 00:02:48 +0100

    detection

---
+++ detection.py
@@ -0,0 +1,24 @@
+import easyocr
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def detect_text(img:str):
+
+    reader = easyocr.Reader(['en'])
+    resualt = reader.readtext(img, detail=0, batch_size=1, paragraph=True)
+    
+    return resualt
+
+
+
+def get_text(img:str):
+    x = detect_text(img)
+    y = ''
+    for i in x:
+        y += i + '\n'
+        
+    return y
+        
+    
+
